614af7ca5562c7b0f6e4341956cdac779729b5f4
noxadev26 <noxadev.id@gmail.com>
2026-07-11 02:29:24 +0700

    Add App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Navbar from './components/Navbar'
+import Home from './components/Home'
+import Footer from './components/Footer'
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <Home />
+      <Footer />
+    </div>
+  )
+}
+export default App
