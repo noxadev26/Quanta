eb62bc394d57ccd156f73181bc0e395fcbc7983e
noxadev26 <noxadev.id@gmail.com>
2026-07-12 00:13:04 +0700

    Delete Footer.jsx

--- src/components/Footer.jsx
+++
@@ -1,8 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="glass mt-20 mx-6 mb-6 rounded-2xl p-6 text-center text-gray-400">
-      <p>© 2026 QUANTA - SSI. All rights reserved.</p>
-      <p className="text-sm mt-2 text-[#00BFFF]">Built with Glassmorphic UI</p>
-    </footer>
-  )
-}
